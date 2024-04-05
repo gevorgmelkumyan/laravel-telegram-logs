6497f39a61e16e7433b64bb7eddf3dc440ac81da
Gevorg Melkumyan <gevorgmelkoumyan@gmail.com>
2024-04-05 16:19:21 +0400

    Removed the unnecessary function use

--- app/Console/Commands/GenerateLogs.php
+++ app/Console/Commands/GenerateLogs.php
@@ -5,7 +5,6 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
-use function Laravel\Prompts\text;
 
 class GenerateLogs extends Command {
     protected $signature = 'app:generate-logs {--count=}';
